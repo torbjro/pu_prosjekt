24c145a2ed58178431b15d79792e1884c604006e
torbjrol <torbjrol@stud.ntnu.no>
2023-02-16 20:42:09 +0100

    Fix pipeline error

--- components/stories/Dashboard/Dashboard.tsx
+++ components/stories/Dashboard/Dashboard.tsx
@@ -72,7 +72,7 @@ export default function Dashboard() {
                   {/* Right section on desktop */}
                   <div className="hidden lg:ml-4 lg:flex lg:items-center lg:pr-0.5">
                     <button
-                      type="button"s
+                      type="button"
                       className="flex-shrink-0 rounded-full p-1 text-indigo-200 hover:bg-white hover:bg-opacity-10 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
                     >
                       <span>Log out</span>
