f6724e6d2bfc0906a2d37bb585eb4ae6a0c4ba3e
Karoline Brocks <karolabr@stud.ntnu.no>
2023-02-22 17:09:48 +0100

    profile backend

---
+++ pages/profile/index.tsx
@@ -0,0 +1,17 @@
+import { pocketbase } from '../api/connects';
+import { Fragment } from 'react'
+
+const userNavigation = [
+    { name: 'Profile', href: '#', current: true },
+    { name: 'Uploaded exercises', href: '#', current: false },
+    { name: 'Liked exercises', href: '#', current: true },
+    { name: 'My progress', href: '#', current: false },
+    { name: 'Weekly plan', href: '#', current: false },
+  ]
+
+const Navigation = [
+    { name: 'Home', href: '#', current: true },
+    { name: 'Groups', href: '#', current: false },
+  ]
+
+  
