0647ab46e66f59231ba9e2969b3214d5357178cb
sofieky <sofieky@stud.ntnu.no>
2023-02-22 16:09:53 +0100

    Added navbar

--- pages/friends/index.tsx
+++ pages/friends/index.tsx
@@ -1,10 +1,15 @@
 import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/20/solid'
-import { ArrowRightOnRectangleIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/outline'
 import { Record } from 'pocketbase'
 import { useEffect, useState } from 'react'
 import { getFriends, pocketbase } from '../api/connects'
+import { Fragment } from 'react'
+import { Menu, Popover, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
 export default function Friends() {
+  const user = pocketbase.authStore.model;
     
     const [friends, setFriends] = useState<Record[]>()
 
@@ -23,10 +28,240 @@ export default function Friends() {
     const goToPage = (friend: Record) => {
         console.log('go to page', friend)
     }
+    const navigation = [
+      { name: 'Home', href: '/dashboard', current: true },
+      { name: 'Groups', href: '#', current: false },
+      { name: 'Friends', href: '/friends', current: false },
+      { name: 'Your pictures', href: '#', current: false },
+      { name: 'Profile', href: '#', current: false },
+    ]
+    const userNavigation = [
+      { name: 'Your Profile', href: '#' },
+      { name: 'Settings', href: '#' },
+      { name: 'Sign out', href: './login' },
+    ]
+    
+    function classNames(...classes: string[]) {
+      return classes.filter(Boolean).join(' ')
+    }
 
     return (
     
-    <form>   
+    <form> 
+      <div className="min-h-full">
+        <Popover as="header" className="bg-violet-600 pb-2">
+          {({ open }) => (
+            <>
+              <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div className="relative flex items-center justify-center py-5 lg:justify-between">
+                  {/* Logo */}
+                  <div className="absolute left-0 flex-shrink-0 lg:static">
+                    <a href="#">
+                      <span className="sr-only">MuscleMates</span>
+                      <img
+                        className="h-8 w-auto"
+                        src="./logo2.svg"
+                        alt="MuscleMates"
+                      />
+                    </a>
+                  </div>
+
+                  {/* Right section on desktop */}
+                  <div className="hidden lg:ml-4 lg:flex lg:items-center lg:pr-0.5">
+                    <button
+                      type="button"
+                      className="flex-shrink-0 rounded-full p-1 text-indigo-200 hover:bg-white hover:bg-opacity-10 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                    >
+                      <span>Log out</span>
+                    {/*on click log out!*/}
+                    </button>
+
+                    {/* Profile dropdown */}
+                    <Menu as="div" className="relative ml-4 flex-shrink-0">
+                      <div>
+                        <Menu.Button className="flex rounded-full bg-white text-sm ring-2 ring-white ring-opacity-20 focus:outline-none focus:ring-opacity-100">
+                          <span className="sr-only">Open user menu</span>
+                          <img className="h-8 w-8 rounded-full" src={user?.imageUrl} alt="" />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute -right-2 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {userNavigation.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    active ? 'bg-gray-100' : '',
+                                    'block px-4 py-2 text-sm text-gray-700'
+                                  )}
+                                >
+                                  {item.name}
+                                </a>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  </div>
+
+                  {/* Menu button */}
+                  <div className="absolute right-0 flex-shrink-0 lg:hidden">
+                    {/* Mobile menu button */}
+                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-transparent p-2 text-indigo-200 hover:bg-white hover:bg-opacity-10 hover:text-white focus:outline-none focus:ring-2 focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="hidden border-t border-white border-opacity-20 py-5 lg:block">
+                  <div className="grid grid-cols-3 items-center gap-8">
+                    <div className="col-span-2">
+                      <nav className="flex space-x-4">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current ? 'text-white' : 'text-indigo-100',
+                              'text-sm font-medium rounded-md bg-white bg-opacity-0 px-3 py-2 hover:bg-opacity-10'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </nav>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <Transition.Root as={Fragment}>
+                <div className="lg:hidden">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="duration-150 ease-out"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="duration-150 ease-in"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Popover.Overlay className="fixed inset-0 z-20 bg-black bg-opacity-25" />
+                  </Transition.Child>
+
+                  <Transition.Child
+                    as={Fragment}
+                    enter="duration-150 ease-out"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="duration-150 ease-in"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                  >
+                    <Popover.Panel
+                      focus
+                      className="absolute inset-x-0 top-0 z-30 mx-auto w-full max-w-3xl origin-top transform p-2 transition"
+                    >
+                      <div className="divide-y divide-gray-200 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                        <div className="pt-3 pb-2">
+                          <div className="flex items-center justify-between px-4">
+                            <div>
+                              <img
+                                className="h-8 w-auto"
+                                src="./logo2.svg"
+                                alt="MuscleMates"
+                              />
+                            </div>
+                            <div className="-mr-2">
+                              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                              </Popover.Button>
+                            </div>
+                          </div>
+                          <div className="mt-3 space-y-1 px-2">
+                            <a
+                              href="#"
+                              className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                            >
+                              <span>Home</span>
+                            </a>
+                            <a
+                              href="#"
+                              className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                            >
+                              <span>Profile</span>
+                            </a>
+                            <a
+                              href="#"
+                              className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                            >
+                              <span>Groups</span>
+                            </a>
+                            <a
+                              href="#"
+                              className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                            >
+                              Your pictures
+                            </a>
+                            <a
+                              href="#"
+                              className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                            >
+                              Friends
+                            </a>
+                          </div>
+                        </div>
+                        <div className="pt-4 pb-2">
+                          <div className="flex items-center px-5">
+                            <div className="flex-shrink-0">
+                              <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                            </div>
+                            <div className="ml-3 min-w-0 flex-1">
+                              <div className="truncate text-base font-medium text-gray-800">{user.name}</div>
+                              <div className="truncate text-sm font-medium text-gray-500">{user.email}</div>
+                            </div>
+                            <button
+                              type="button"
+                              className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                              <span className="sr-only">View notifications</span>
+                              <BellIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                          </div>
+                          <div className="mt-3 space-y-1 px-2">
+                            {userNavigation.map((item) => (
+                              <a
+                                key={item.name}
+                                href={item.href}
+                                className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                              >
+                                {item.name}
+                              </a>
+                            ))}
+                          </div>
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition.Child>
+                </div>
+              </Transition.Root>
+            </>
+          )}
+        </Popover>
+        <div className="pt-2"></div>
       <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
       <div className="relative">
       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -35,7 +270,8 @@ export default function Friends() {
       <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-violet-500 focus:border-violet-500 dark:bg-white dark:border-violet-600 dark:placeholder-violet-600 dark:text-black dark:focus:ring-violet-500 dark:focus:border-violet-500" placeholder="Search for friends" required></input>
       <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-500 font-medium rounded-lg text-sm px-4 py-2 dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-500">Search</button>
       </div>
-        <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="pt-10"></div>
+        <ul role="list" className="px-5 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {friends?.map((friend) => (
           <li
           key={friend.id}
@@ -59,7 +295,6 @@ export default function Friends() {
               <div className="-ml-px flex w-0 flex-1">
                 <button
                 onClick={() => handleUnfollow(friend)}
-
                   className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
                 >
                   <XMarkIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -71,6 +306,16 @@ export default function Friends() {
         </li>
       ))}
     </ul>
+        <footer>
+          <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
+              <span className="block sm:inline">&copy; 2023 MuscleMates, Inc.</span>{' '}
+              <span className="block sm:inline">All rights reserved.</span>
+            </div>
+          </div>
+        </footer>
+      </div>
+      
     </form>
   )
 }
