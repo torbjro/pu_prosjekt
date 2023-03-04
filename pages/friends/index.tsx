574875d2cd66623b601d76c4c971cbf37de11abd
sofieky <sofieky@stud.ntnu.no>
2023-03-04 12:58:25 +0100

    Implemented follow and unfollow. Co-Authored by @torbjrol

--- pages/friends/index.tsx
+++ pages/friends/index.tsx
@@ -6,6 +6,7 @@ import { Fragment } from 'react'
 import { Menu, Popover, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Router } from 'next/router'
+import { filter } from '@chakra-ui/react'
 
 export default function Friends() {
     const user = pocketbase.authStore.model;
@@ -21,27 +22,45 @@ export default function Friends() {
         })
     }, [])
 
-    console.log('friends', friends)
-
-    const handleUnfollow = async (friend: Record) => {
-        const oldFriends = pocketbase.authStore.model?.friends;
-
-
-        const newFriends = oldFriends?.filter((f: Record) => f !== friend)
-        console.log(newFriends)
+    const handleUnfollow = async (f: Record) => {
+        const oldFriendsId = user?.friends?.map((oldFriend: Record) => oldFriend)
+        const newFriends = oldFriendsId?.filter((oldFriend: string) => oldFriend !== f.id)
         const newFriend = await pocketbase.collection('users').update(pocketbase.authStore.model!.id, { friends: newFriends })
         location.reload()
     }
-    const goToPage = (friend: Record) => {
-        console.log('go to page', friend)
+
+    const handleFollow = async (f: Record) => {
+        const oldFriends = user?.friends?.map((oldFriend: Record) => oldFriend)
+        const newFriends = oldFriends?.push(f)
+        const newFriend = await pocketbase.collection('users').update(pocketbase.authStore.model!.id, { friends: newFriends })
+        location.reload()
     }
+
+    const handleSearch = async (search: string) => {
+        //console.log(e)
+       //search.preventDefault()
+        try {
+            const users = await pocketbase.collection('users').getFullList()
+            const userss = users?.map((user: Record) => user)
+            const searched = userss?.filter((user: Record) => user.name == search)
+            console.log(searched)
+            await handleFollow(searched[0])
+            //const friend = await pocketbase.collection('users').getOne(`${e}`)
+            //console.log(users)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const [search, setSearch] = useState<string>('')
+        
     return (
 
         <form>
             <div className="min-h-full">
                 <div className="relative px-5">
-                    <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-violet-500 focus:border-violet-500 dark:bg-white dark:border-violet-600 dark:placeholder-violet-600 dark:text-black dark:focus:ring-violet-500 dark:focus:border-violet-500" placeholder="Search for friends" required></input>
-                    <button type="submit" className="text-white absolute right-7 bottom-2.5 bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-500 font-medium rounded-lg text-sm px-4 py-2 dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-500">Search</button>
+                    <input type="search" onChange={(e) => setSearch(e.target.value)} id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-violet-500 focus:border-violet-500 dark:bg-white dark:border-violet-600 dark:placeholder-violet-600 dark:text-black dark:focus:ring-violet-500 dark:focus:border-violet-500" placeholder="Search for friends" ></input>
+                    <button onClick={() => handleSearch(search)} type="submit" className="text-white absolute right-7 bottom-2.5 bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-500 font-medium rounded-lg text-sm px-4 py-2 dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-500">Search</button>
                 </div>
                 <ul role="list" className="px-5 mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
                     {friends?.items?.map((friend) => (
@@ -57,19 +76,12 @@ export default function Friends() {
                                 <div className=" flex divide-x divide-gray-200">
                                     <div className="flex w-0 flex-1">
                                         <button
-                                            onClick={() => goToPage(friend)}
-                                            className="relative inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
-                                            <ArrowRightOnRectangleIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                            <span className="mx-3">Go to profile</span>
-                                        </button>
-                                    </div>
-                                    <div className="flex w-0 flex-1">
-                                        <button
-                                            onClick={() => handleUnfollow(friend)}
+                                            
                                             className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                                        >
+                                            onClick={() => handleUnfollow(friend)}
+                                        >   
                                             <XMarkIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                            <span className="mx-3">Unfollow 🎃</span>
+                                            <span className="mx-3">Unfollow</span>
                                         </button>
                                     </div>
                                 </div>
