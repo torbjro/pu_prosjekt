8dc263fd566b14de20286414babe2ba2ac2c7079
Karoline Brocks <karolabr@stud.ntnu.no>
2023-03-07 15:28:24 +0100

    color

--- components/stories/Groupbio/Groupbio.tsx
+++ components/stories/Groupbio/Groupbio.tsx
@@ -12,7 +12,7 @@ const Groupbio: FC<GroupbioProps> = (props) => {
         //Groupbio tailwind style
         <a className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-violet-800 dark:border-violet-700 dark:hover:bg-violet-700">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{groupName}</h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">{bio}</p>
+        <p className="font-normal text-gray-200 dark:text-gray-200">{bio}</p>
         </a>
     )
 }
